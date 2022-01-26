d3f81067d566d5a6664c138b0eaec20238fb196a
Svetoslav Zhekov <svetoslav.zhekov@abv.bg>
2022-01-27 01:48:15 +0200

    Added new game button click sound.

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,16 @@
-//TO DO
 //Getting Button And Sound By ID
-var button = document.getElementById("ngButton");
+var button = document.getElementById("button");
 var sound = document.getElementById("ngSound");
+
+//Sound Set Up
+sound.volume = 0.5;
 
 //On Window Load, Set Button To Play Sound On Click
 window.onload = function() {
     button.onclick = function() {
         sound.play();
-        sound.addEventListener("ended", function(){ 
-            button.click;
+        sound.addEventListener("ended", function() {
+            window.location.href = "main.html";
         });
     }
 }
